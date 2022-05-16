f2bae583fdc2cb57d979d760077284dc674af794
MARI150994 <mari.utkina.94@mail.ru>
2022-05-16 13:14:36 +0300

    change tasks for celery

--- tasksys/task/tasks.py
+++ tasksys/task/tasks.py
@@ -2,11 +2,17 @@ from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.core.mail import EmailMessage
 
+from django.utils import timezone
+
 
 logger = get_task_logger(__name__)
 
 
 @shared_task
 def send_mail_task(subject, message, users):
+    logger.info(f'Send mail task to {users}, text: {message}, time: {timezone.now()}')
     email = EmailMessage(subject, message, to=[users])
     email.send()
+    logger.info('tasks finished')
+
+
